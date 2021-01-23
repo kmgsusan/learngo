8755f59bef682b03ead6d94ae155c13e669396c0
kmgsusan <kmgsusan82@gmail.com>
2021-01-23 17:11:32 +0900

    3.1

--- 3.0/main.go
+++ 3.0/main.go
@@ -6,11 +6,11 @@ import (
 	"net/http"
 )
 
-// errHttpNotFound
-var errHttpNotFound = errors.New("http not found.")
+var errRequestFailed = errors.New("Request failed")
 
 // main
 func main() {
+	var results = make(map[string]string)
 	urls := []string{
 		"https://www.airbnb.com/",
 		"https://www.google.com/",
@@ -21,22 +21,30 @@ func main() {
 		"https://www.facebook.com/",
 		"https://www.instagram.com/",
 		"https://academy.nomadcoders.co/",
-		"https://mgkim.cafe24.com/",
-		"http://www.xxdee.dfd.com/",
 	}
 	for _, url := range urls {
-		fmt.Println(url)
-		hitUrl(url)
+		// fmt.Println(url)
+		result := "OK"
+		err := hitURL(url)
+		if err != nil {
+			result = "FAILED"
+		}
+		results[url] = result
+	}
+
+	for url, result := range results {
+		fmt.Println(url, result)
 	}
 }
 
-// hitUrl
-func hitUrl(url string) error {
+// hitURL x
+func hitURL(url string) error {
 	resp, err := http.Get(url)
-	fmt.Println(resp.StatusCode)
-	fmt.Println(resp.Body)
+	// fmt.Println(resp.StatusCode)
+	// fmt.Println(resp.Body)
 	if err != nil || resp.StatusCode >= 400 {
-		return errHttpNotFound
+		fmt.Println(url, err, resp.StatusCode)
+		return errRequestFailed
 	}
 	return nil
 }
